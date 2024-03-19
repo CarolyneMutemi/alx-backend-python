bf5eb9fc28711e082b47f28b4c9f82da3ac04464
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-19 17:14:09 +0300

    Changed the flow

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ import asyncio
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, any]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Yields a random number between 0 and 10.
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
