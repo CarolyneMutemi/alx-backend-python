2f3971fda14b2e88bb308c8a935809045948f67d
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-08 02:13:03 +0300

    Testing that the utils.access_nested_map method raises KeyError when the error occurs.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,15 @@ class TestAccessNestedMap(unittest.TestCase):
         given nested maps and different paths.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, error_message):
+        """
+        Tests that the function access_nested_map raises a KeyError
+        when the path parameters are not found in the nested_map.
+        """
+        with self.assertRaises(KeyError, msg=error_message):
+            access_nested_map(nested_map, path)
